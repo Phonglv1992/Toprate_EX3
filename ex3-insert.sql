bcb3838c1e31a078eee8d7c3947a474e8df20184
Phonglv1992 <phonghe.info@gmail.com>
2019-06-05 13:45:25 +0700

    first commit

---
+++ ex3-insert.sql
@@ -0,0 +1,107 @@
+﻿
+insert into KHACH_HANG values('viettel',N'công ty viettel','viettel','ha noi','viettel@gmail.com','022888.888','');
+insert into KHACH_HANG values('tgdd',N'công ty tgdd','tgdd','ha noi','tgdd@gmail.com','789674','');
+-------
+insert into KHACH_HANG values('KH1',N'','MUA SIM',N'ha noi','KH1@GMAIL.COM','94354686','');
+insert into KHACH_HANG values('KH2',N'','MUA DT',N'tp hcm','KH2@GMAIL.COM','45464646','');
+insert into KHACH_HANG values('KH3',N'TOPRATE','MUA LAP TOP',N'ha noi','KH3@GMAIL.COM','445445555','');
+insert into KHACH_HANG values('KH4',N'ABC','MUA DT',N'da nang','KH4@GMAIL.COM','8765','');
+insert into KHACH_HANG values('KH5',N'XYZ','MUA LAP TOP',N'HOANG MAI','kh5@GMAIL.COM','6443554','');
+insert into KHACH_HANG values('KH6',N'DBC','MUA DT',N'HOANG MAI','KH6@GMAIL.COM','55434343','');
+insert into KHACH_HANG values('KH7',N'','MUA SIM',N'HOANG MAI','KH7@GMAIL.COM','0973554686','');
+insert into KHACH_HANG values('KH8',N'','MUA LAP TOP',N'ha noi','kh8@GMAIL.COM','','');
+insert into KHACH_HANG values('KH9',N'','MUA DT',N'ha noi','KH9@GMAIL.COM','','');
+insert into KHACH_HANG values('KH10',N'','MUA SIM',N'ha noi','KH10@GMAIL.COM','','');
+insert into KHACH_HANG values('KH11',N'','MUA DT',N'nghi xuan','KH11@GMAIL.COM','','');
+
+select * from KHACH_HANG;
+---------------------------------------------------------------------------------------------------
+insert into NHA_CC values('tgdd',N'công ty tgdd ','tgdd','ha noi','789674','','tgdt@gmail.com');
+insert into NHA_CC values('viettel',N'công ty viettel ','viettel','ha noi','022888.888','','viettel@gmail.com');
+insert into NHA_CC values('fpt',N'công ty viettel DN','fpt','da nang','02242424','','fpt@gmail.com');
+insert into NHA_CC values('vinaphone',N'công ty vinaphone','vinaphone',N'hcm','02223242','','vinaphone@gmail.com');
+insert into NHA_CC values('mobilephone',N'công ty mobilephone','mobilephone','ha noi','02666666','','mobilephone@gmail.com');
+insert into NHA_CC values('dg',N'dầu gội đầu','dau goi',N'ha noi','02234347','45646664','daugoiHN@gmail.com');
+
+--------------------------------------------------------------------------------------------------------
+insert into NHAN_VIEN values('NV1',N'Lê',N'Phong','9/15/1976','7/25/2000',N'Hà tĩnh','0973554686','2000000','1000000');
+insert into NHAN_VIEN values('NV2',N'Nguyễn',N'A','8/7/1970','1/16/2003',N'Hà Nam','0912123998','2500000','300000');
+insert into NHAN_VIEN values('NV3',N'Trần',N'B','1/13/1978','03/05/2000',N'Phú thọ','097234434','6000000','1000000');
+insert into NHAN_VIEN values('NV4',N'Lê',N'C','5/12/1979','1/6/2001',N'Hà Tây','123456789','5500000','150000');
+insert into NHAN_VIEN values('NV5',N'Phan',N'D','12/12/1980','7/27/2001',N'Thái bình','98765433','12000000','4000000');
+insert into NHAN_VIEN values('NV6',N'Tô',N'E','2/2/1990','2/24/2000',N'Hà nội','876545786','8500000','300000');
+insert into NHAN_VIEN values('NV7',N'Lê',N'A','1/1/1980','7/25/2002',N'Hà nội','345674567','4000000','800000');
+insert into NHAN_VIEN values('NV8',N'A',N'B','5/12/1979','1/6/2001',N'Hà Tây','123456789','5500000','150000');
+insert into NHAN_VIEN values('NV9',N'Đinh',N'Lợi','12/12/1980','7/27/2001',N'Thái bình','98765433','12000000','4000000');
+insert into NHAN_VIEN values('NV10',N'Trần',N'Choai','2/2/1982','2/24/2000',N'Hà nội','876545786','8500000','300000');
+
+         ----------------------------------------------------------------------------------------------------
+insert into LOAI_HANG values('DG',N'dầu gội');
+insert into LOAI_HANG values('DT',N'điện thoại');
+insert into LOAI_HANG values('LT',N'laptop');
+insert into LOAI_HANG values('MT',N'mỳ tôm');
+insert into LOAI_HANG values('OTO',N'xe oto');
+insert into LOAI_HANG values('TL',N'thuốc lá');
+insert into LOAI_HANG values('XM',N'xe máy');
+INSERT INTO LOAI_HANG VALUES('SIM',N'sim điện thoại');
+         ------------------------------------------------------------------------------------------------
+insert into MAT_HANG values('VT5s',N'iphone 5s','viettel','DT','20','vnđ','1200000');
+insert into MAT_HANG values('VT6s',N'iphone 6s','viettel','DT','5','vnđ','700000');
+insert into MAT_HANG values('VT7s',N'iphone 7s','viettel','DT','10','vnđ','12000000');
+insert into MAT_HANG values('VT8s',N'iphone 8s','viettel','DT','16','vnđ','1700000');
+insert into MAT_HANG values('VTs5',N'galaxy s5','viettel','DT','10','vnđ','200000');
+insert into MAT_HANG values('VTs6',N'galaxy s6','viettel','DT','15','vnđ','500000');
+insert into MAT_HANG values('VTs7',N'galaxy s7','viettel','DT','50','vnđ','9200000');
+insert into MAT_HANG values('VTs8',N'galaxy s8','viettel','DT','8','vnđ','1400000');
+insert into MAT_HANG values('VTs9',N'galaxy s9','viettel','DT','11','vnđ','2000000');
+insert into MAT_HANG values('VTnote6',N'iphone note 6','viettel','DT','10','vnđ','2500000');
+-------
+insert into MAT_HANG values('TGDD5s',N'iphone 5s','tgdd','DT','20','vnđ','1400000');
+insert into MAT_HANG values('TGDD6s',N'iphone 6s','tgdd','DT','5','vnđ','7200000');
+insert into MAT_HANG values('TGDD7s',N'iphone 7s','tgdd','DT','10','vnđ','1200000');
+insert into MAT_HANG values('TGDD8s',N'iphone 8s','tgdd','DT','16','vnđ','17000000');
+insert into MAT_HANG values('TGDDs5',N'galaxy s5','tgdd','DT','10','vnđ','2000000');
+insert into MAT_HANG values('TGDDs6',N'galaxy s6','tgdd','DT','15','vnđ','5000000');
+insert into MAT_HANG values('TGDDs7',N'galaxy s7','tgdd','DT','50','vnđ','9200000');
+insert into MAT_HANG values('TGDDs8',N'galaxy s8','tgdd','DT','8','vnđ','14000000');
+insert into MAT_HANG values('TGDDs9',N'galaxy s9','tgdd','DT','11','vnđ','20000000');
+insert into MAT_HANG values('TGDDnote6',N'iphone note 6','tgdd','DT','10','vnđ','2500000');
+-----------------------------------------------
+
+
+
+insert into DON_DAT_HANG values('01','KH1','NV1','2/19/2002','2/19/2002','2/19/2002',N'ha noi');
+insert into DON_DAT_HANG values('02','KH2','NV2','12/4/2003','12/4/2003','12/4/2003',N'hcm');
+insert into DON_DAT_HANG values('03','KH3','NV2','2/19/2003','2/19/2003','2/19/2003',N'ha noi');
+insert into DON_DAT_HANG values('04','KH4','NV3','5/1/2000','5/1/2000','5/1/2000',N'da nang');
+insert into DON_DAT_HANG values('05','KH5','NV4','6/15/2000','6/15/2000','6/15/2000',N'ha nam');
+insert into DON_DAT_HANG values('06','KH6','NV2','1/29/2003','1/29/2003','1/29/2003',N'hai phong');
+insert into DON_DAT_HANG values('07','KH7','NV2','2/24/2003','2/24/2003','2/24/2003',N'hcm');
+insert into DON_DAT_HANG values('08','KH8','NV3','3/2/2003','3/2/2003','3/2/2003',N'ha noi')
+insert into DON_DAT_HANG values('09','KH9','NV3','3/24/2003','3/24/2003','3/24/2003',N'da nang');
+insert into DON_DAT_HANG values('010','KH10','NV6','3/4/2003','3/4/2003','3/4/2003',N'ha tinh');
+insert into DON_DAT_HANG values('011','KH11','NV6','3/17/2002','3/17/2002','3/17/2002',N'');
+
+---------------------------
+
+
+insert into CHI_TIET_DAT_HANG values('03','VT5s','1200000','1','0');
+insert into CHI_TIET_DAT_HANG values('02','VT5s','1200000','8','0');
+insert into CHI_TIET_DAT_HANG values('01','TGDD6s','14000000','2','0');
+insert into CHI_TIET_DAT_HANG values('07','TGDD6s','14000000','3','0');
+insert into CHI_TIET_DAT_HANG values('03','VT6s','7000000','1','0');
+insert into CHI_TIET_DAT_HANG values('02','VT6s','7000000','2','0');
+insert into CHI_TIET_DAT_HANG values('08','TGDD6s','7000000','4','0');
+insert into CHI_TIET_DAT_HANG values('09','VT6s','7000000','1','0');
+insert into CHI_TIET_DAT_HANG values('10','VT6s','7000000','2','0');
+
+select * from CHI_TIET_DAT_HANG;
+
+/*delete from CHI_TIET_DAT_HANG;
+delete from MAT_HANG;
+delete from DON_DAT_HANG;
+delete from NHA_CC;
+delete from NHAN_VIEN;
+delete from KHACH_HANG;
+delete from LOAI_HANG;
+*/
---
+++ select-ex3.sql
@@ -0,0 +1,132 @@
+﻿--1. Hãy cho biết có những khách hàng nào lại chính là đối tác cung cấp hàng của công ty(tức là có cùng tên giao dịch).
+select kh.MA_KH,kh.TEN_CTY,kh.TEN_GIAO_DICH from KHACH_HANG kh
+join NHA_CC ncc on ncc.TEN_GIAO_DICH=kh.TEN_GIAO_DICH;
+--2. Những đơn đặt hàng nào yêu cầu giao hàng ngay tại cty đặt hàng và những đơn đó là của công ty nào? 
+select ddh.SO_HOA_DON,ddh.NOI_GIAO_HANG,kh.TEN_GIAO_DICH from DON_DAT_HANG ddh
+join KHACH_HANG kh on kh.MA_KH=ddh.MA_KH where kh.DIA_CHI=ddh.NOI_GIAO_HANG;
+--3.Những mặt hàng nào chưa từng được khách hàng đặt mua?
+select MA_HANG,TEN_HANG from MAT_HANG
+where not EXISTS (select MA_HANG from CHI_TIET_DAT_HANG where CHI_TIET_DAT_HANG.MA_HANG=MAT_HANG.MA_HANG); 
+--4. Những nhân viên nào của công ty chưa từng lập bất kỳ một hoá đơn đặt hàng nào?
+select * from NHAN_VIEN
+where NHAN_VIEN.MA_NV NOT IN (select distinct(MA_NV) from DON_DAT_HANG);
+--5. Trong năm 2003, những mặt hàng nào chỉ được đặt mua đúng một lần
+SELECT count(MAT_HANG.MA_HANG) 'số lượng',TEN_HANG 'Tên mặt hàng' from (
+MAT_HANG join CHI_TIET_DAT_HANG on MAT_HANG.MA_HANG=CHI_TIET_DAT_HANG.MA_HANG)
+join DON_DAT_HANG on DON_DAT_HANG.SO_HOA_DON=CHI_TIET_DAT_HANG.SO_HOA_DON
+where YEAR (DON_DAT_HANG.NGAY_DAT_HANG)=2003 
+group by (MAT_HANG.TEN_HANG)
+having count(MAT_HANG.MA_HANG)=2;
+
+--select * from DON_DAT_HANG where YEAR(DON_DAT_HANG.NGAY_DAT_HANG)=2003;
+--6. Hãy cho biết mỗi một khách hàng đã phải bỏ ra bao nhiêu tiền để đặt mua hàng củacông ty? 
+select KHACH_HANG.MA_KH, sum(SO_LUONG*GIA_BAN - SO_LUONG*GIA_BAN*MUC_GIAM_GIA/100) from KHACH_HANG
+join DON_DAT_HANG on DON_DAT_HANG.MA_KH=KHACH_HANG.MA_KH
+join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.SO_HOA_DON=DON_DAT_HANG.SO_HOA_DON
+group by (KHACH_HANG.MA_KH);
+--7. Mỗi một nhân viên của công ty đã lập bao nhiêu đơn đặt hàng
+-- (nếu nhân viên chưahề lập một hoá đơn nào thì cho kết quả là 0)
+
+
+select NHAN_VIEN.HO,NHAN_VIEN.TEN,count(DON_DAT_HANG.MA_NV) 'số lượng' from NHAN_VIEN
+left join DON_DAT_HANG on DON_DAT_HANG.MA_NV=NHAN_VIEN.MA_NV
+group by NHAN_VIEN.HO, NHAN_VIEN.TEN;
+--8. Cho biết tổng số tiền hàng mà cửa hàng thu được trong mỗi tháng của năm 2003
+--(thời được gian tính theo ngày đặt hàng).
+select Month(DON_DAT_HANG.NGAY_DAT_HANG) 'Tháng', 
+sum(CHI_TIET_DAT_HANG.GIA_BAN*SO_LUONG-CHI_TIET_DAT_HANG.GIA_BAN*SO_LUONG*MUC_GIAM_GIA/100) 'Tổng số tiền bán' 
+from (DON_DAT_HANG join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.SO_HOA_DON=DON_DAT_HANG.SO_HOA_DON)
+where YEAR (NGAY_DAT_HANG)=2003
+group by(Month(DON_DAT_HANG.NGAY_DAT_HANG));
+
+--9. Hãy cho biết tổng số lượng hàng của mỗi mặt hàng mà cty đã có (tổng số lượng hàng hiện có và đã bán).
+-- cách 1:
+select MAT_HANG.MA_HANG ,MAT_HANG.SO_LUONG,CHI_TIET_DAT_HANG.SO_LUONG, MAT_HANG.SO_LUONG+
+CASE
+   WHEN SUM(CHI_TIET_DAT_HANG.SO_LUONG) IS NULL THEN 0
+	ELSE
+	SUM(CHI_TIET_DAT_HANG.SO_LUONG)
+	 END AS TONG
+from MAT_HANG
+left join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.MA_HANG=MAT_HANG.MA_HANG
+group by MAT_HANG.MA_HANG, MAT_HANG.SO_LUONG, CHI_TIET_DAT_HANG.SO_LUONG
+--cách 2:
+
+select MAT_HANG.MA_HANG,MAT_HANG.SO_LUONG,CHI_TIET_DAT_HANG.SO_LUONG ,SUM(MAT_HANG.SO_LUONG + isNull(CHI_TIET_DAT_HANG.SO_LUONG,0))
+from MAT_HANG
+left join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.MA_HANG=MAT_HANG.MA_HANG
+group by MAT_HANG.MA_HANG, MAT_HANG.SO_LUONG, CHI_TIET_DAT_HANG.SO_LUONG
+--10. Nhân viên nào của cty bán được số lượng hàng nhiều nhất và số lượng hàng bán được
+--của nhân viên này là bao nhiêu?
+
+select top(1) NHAN_VIEN.MA_NV,NHAN_VIEN.HO,NHAN_VIEN.TEN,sum(CHI_TIET_DAT_HANG.SO_LUONG) SL from NHAN_VIEN
+join DON_DAT_HANG on DON_DAT_HANG.MA_NV=NHAN_VIEN.MA_NV
+join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.SO_HOA_DON=DON_DAT_HANG.SO_HOA_DON
+group by NHAN_VIEN.MA_NV,NHAN_VIEN.HO,NHAN_VIEN.TEN
+order by SL desc
+--11. Mỗi một đơn đặt hàng đặt mua những mặt hàng nào 
+--và tổng số tiền mà mỗi đơn đặt hàng phải trả là bao nhiêu?
+select DON_DAT_HANG.SO_HOA_DON,sum(CHI_TIET_DAT_HANG.SO_LUONG*GIA_BAN) from DON_DAT_HANG
+join CHI_TIET_DAT_HANG on CHI_TIET_DAT_HANG.SO_HOA_DON=DON_DAT_HANG.SO_HOA_DON
+join MAT_HANG on MAT_HANG.MA_HANG=CHI_TIET_DAT_HANG.MA_HANG
+group by(DON_DAT_HANG.SO_HOA_DON)
+
+--12. Hãy cho biết mỗi một loại hàng bao gồm những mặt hàng nào, tổng số lượng hàng của mỗi loại 
+--và tổng số lượng của tất cả các mặt hàng hiện có trong công ty là bao nhiêu?
+
+
+--ạo thủ tục lưu trữ để thông qua thủ tục này có thể bổ sung thêm một bản ghi mớicho bảng MATHANG
+-- (thủ tục phải thực hiện kiểm tra tính hợp lệ của dữ liệu cần bổsung: 
+--không trùng khoá chính và đảm bảo toàn vẹn tham chiếu)
+
+ALTER PROCEDURE INSERT_MAT_HANG
+	@MA_HANG varchar(10)  = NULL,
+	@TEN_HANG NVARCHAR(100)  = NULL,
+	@MA_CTY VARCHAR(10)  = NULL,
+	@MA_LOAI_HANG NVARCHAR(10) = NULL,
+	@SO_LUONG BIGINT = NULL,
+	@DON_VI_TINH NVARCHAR(20) = NULL,
+	@GIA_HANG MONEY  = NULL
+
+AS
+
+begin
+	SET XACT_ABORT ON
+	SET NOCOUNT ON;
+	BEGIN TRAN
+	BEGIN TRY
+	INSERT INTO MAT_HANG (MA_HANG,TEN_HANG,MA_CTY,MA_LOAI_HANG,SO_LUONG,DON_VI_TINH,GIA_HANG)
+	values(@MA_HANG,@TEN_HANG,@MA_CTY,@MA_LOAI_HANG,@SO_LUONG,@DON_VI_TINH,@GIA_HANG)
+	
+	COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+		DECLARE @ErrorMessage VARCHAR(2000)
+		SELECT @ErrorMessage = 'Lỗi: ' + ERROR_MESSAGE()
+		RAISERROR(@ErrorMessage, 16, 1)
+	END CATCH
+END
+--Tạo thủ tục lưu trữ có chức năng thống kê tổng số lượng hàng bán được của một mặt hàng có mã bất kỳ 
+--(mã mặt hàng cần thống kê là tham số của thủ tục).
+alter procedure Search_Mat_Hang
+@MA_HANG NVARCHAR(10)=NULL
+AS
+SET NOCOUNT ON
+BEGIN
+	
+	--declare @STMT nvarchar(max) 
+	--set @STMT='select CHI_TIET_DAT_HANG.MA_HANG,sum(CHI_TIET_DAT_HANG.SO_LUONG) [số lượng bán ra] from CHI_TIET_DAT_HANG	
+	--	where CHI_TIET_DAT_HANG.MA_HANG='+@MA_HANG+
+	--	'group by CHI_TIET_DAT_HANG.MA_HANG'
+	select CHI_TIET_DAT_HANG.MA_HANG,sum(CHI_TIET_DAT_HANG.SO_LUONG) 'số lượng bán ra' from CHI_TIET_DAT_HANG	
+	where CHI_TIET_DAT_HANG.MA_HANG=@MA_HANG
+	group by CHI_TIET_DAT_HANG.MA_HANG
+
+END	
+--EXEC sp_executesql @STMT
+SET NOCOUNT OFF
+
+
+
+
